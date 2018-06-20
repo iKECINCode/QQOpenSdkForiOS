f5dcfd10f41700f75130538023519d7d500d7fe7
yangheng <yangheng_work@live.com>
2018-06-20 12:45:05 +0800

    示例完善

--- Classes/Test.swift
+++ Classes/Test.swift
@@ -1,6 +1,6 @@
-class Test {
-    
-    class func test() -> String {
+//如果不需要支持oc调用，不用添加@objcMembers
+@objcMembers public class Test {
+    public class func test() -> String {
         return "aaaa"
     }
 }
