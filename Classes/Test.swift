7c440954f49dd07a8bb0d1e828054f0434e70b78
yangheng <yangheng_work@live.com>
2018-06-28 12:18:33 +0800

    支持OC调用Swift

--- Classes/Test.swift
+++ Classes/Test.swift
@@ -1,6 +1,6 @@
-//如果不需要支持oc调用，不用添加@objcMembers
-@objcMembers public class Test {
-    public class func test() -> String {
+//如果不需要支持oc调用，不用继承NSObject与添加@objc
+public class Test :NSObject {
+    @objc public class func test() -> String {
         return "aaaa"
     }
 }
